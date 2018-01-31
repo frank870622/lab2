bb2015da3cd963861a78761415995e66c644d7e3
frank870622 <frank870622@gmail.com>
2018-01-31 11:54:43 +0800

    2018/01/31 Hello World!!

--- lab2.cpp
+++ lab2.cpp
@@ -1,15 +1,50 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
 #include"lab2.h"
+
+A::A(){
+	ans1 = 0;
+	ans2 = 0;
+	K = 0;
+	RA = 0;
+	RB = 0;
+	EA = 0;
+	EB = 0;
+}
+A::A(int a,int b,int c){
+	K = a;
+	RA = b;
+	RB = c;
+	ans1 = 0;
+	ans2 = 0;
+	EA = 0;
+	EB = 0;
+}
+void A::count(float win){
+	EA = 1 / (1 + pow(10, (RB - RA) / 400));
+	EB = 1 / (1 + pow(10, (RA - RB) / 400));
+	RA = RA + K * (win - EA);
+	RB = RB + K * ((1 - win) - EB);
+	RA = floor(RA + 0.5);
+	RB = floor(RB + 0.5);
+	ans1 = (int)RA;
+	ans2 = (int)RB;
+	ofstream outfile("file.out", ios::app);
+	outfile << ans1 << "\t" << ans2 << endl;
+}
+
 int main(){
 	int a,b,c;
-	ifstream infile("file.in",ios::in);
+	ifstream infile("file.in", ios::in);
 	infile >> a >> b >> c;
 	ofstream outfile("file.out",ios::app);
 	outfile << b << "\t" << c << endl;
-	float arr[6];
+	float arr;
 	A A1(a,b,c);
 	for (int i=0;i<6;i++){
-		infile >> arr[i];
-		A1.count(arr[i]);
+		infile >> arr;
+		A1.count(arr);
 	}
 	return 0;
 }
--- lab2.h
+++ lab2.h
@@ -1,30 +1,17 @@
+#ifndef _LAB2_h_
+#define _LAB2_h_
 #include<iostream>
 #include<cmath>
 #include<fstream>
 using namespace std;
 class A{
         public:
-        A();
-        A(int a,int b,int c){
-                K = a;
-                RA = b;
-                RB = c;
-        };
-        void count(float win){
-                        EA = 1/(1+pow(10,(RB-RA)/400));
-                        EB = 1/(1+pow(10,(RA-RB)/400));
-                        RA = RA + K*(win-EA);
-                        RB = RB + K*((1-win)-EB);
-			RA = floor(RA+0.5);
-			RB = floor(RB+0.5);
-                        ans1 = (int)RA;
-                        ans2 = (int)RB;
-                        ofstream outfile("file.out",ios::app);
-                        outfile << ans1 << "\t" << ans2 << endl;
-        }
+        	A();
+        	A(int,int,int);
+        	void count(float);
+
         private:
-        int ans1,ans2;
-        double K,RA,RB,EA,EB;
-
+        	int ans1,ans2;
+        	double K,RA,RB,EA,EB;
 };
-
+#endif
